3da6afcaa4990ce9deef1c2ff7ab5b5bb871ca52
zasalastobon <zas_91@hotmail.com>
2023-11-12 22:53:47 -0500

    Update pagination.vue

--- app/components/commons/pagination.vue
+++ app/components/commons/pagination.vue
@@ -5,13 +5,13 @@
         to="/blog"
         class="-mt-px border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150"
       >
-        <svg class="mr-3 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            fill-rule="evenodd"
-            d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
+       // <svg class="mr-3 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+        //  <path
+        //    fill-rule="evenodd"
+          //  d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+        //    clip-rule="evenodd"
+       //   />
+      //  </svg>
         Previous
       </nuxt-link>
     </div>
